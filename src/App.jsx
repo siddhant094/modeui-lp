5116eb2a808a28b54b9244cc6bb6c46d408fc24c
Siddhant Pandey <pandeysiddhant21@gmail.com>
2024-01-06 11:51:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return (
+        <div className='h-screen'>
+            <div className='h-screen flex justify-center items-center'>
+                <span>ModeUI Landing Page</span>
+            </div>
+        </div>
+    );
+}
+
+export default App;
